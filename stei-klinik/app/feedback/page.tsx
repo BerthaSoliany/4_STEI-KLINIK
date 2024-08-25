155ba8167fb18799e47a410e98ca558c7c99a787
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-08-25 21:32:09 +0700

    update feedback

---
+++ stei-klinik/app/feedback/page.tsx
@@ -0,0 +1,11 @@
+export default function feedback () {
+    return <body>
+        <div className="overscroll-auto bg-[#ffffff] h-screen w-screen">
+            <div className="text-black font-bold text-base" style={{marginLeft:"590px", marginTop:"20px"}}>STEI-KLINIK</div>
+            <div className="text-black ml-[430px]">We’d love your feedback on  the STEI-KLINIK experience</div>
+            <div className="h-[441px] w-[610px] bg-[#DAEED5] rounded-[5px] ml-[345px] mt-[20px]">
+                <div className="text-black font-bold ml-[210px]">How's your experience?</div>
+            </div>
+        </div>
+    </body>
+}
