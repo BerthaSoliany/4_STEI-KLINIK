6ae694cc0c611fbd2bf027d5614cf3cafb98636e
Henry Filberto Shenelo <19623212@std.stei.itb.ac.id>
2024-08-25 20:26:38 +0700

    connect page

--- stei-klinik/app/navbar.tsx
+++ stei-klinik/app/navbar.tsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 export interface navProps {
     gambar: string;
     judul: string;
+    href: string; // Add href prop
 }
 
-const Navbar = ({judul, gambar}: navProps) => {
-    return(
-        <nav className="border-[1px] border-[#006769] rounded-[10px] h-[40px] w-[290px] mt-5 bg-[#ffffff]" style={{marginLeft:"33px"}}>
-            <Link href="/">
-            <div className="grid grid-cols-11">
-                <img src={gambar} alt="chat" style={{width:"23px", height:"23px", marginLeft:"20px", marginTop:"7px"}}/>
-                <p className="text-black" style={{marginLeft:"40px", marginTop:"8px"}}>{judul}</p>
-            </div>
+const Navbar = ({ judul, gambar, href }: navProps) => {
+    return (
+        <nav className="border-[1px] border-[#006769] rounded-[10px] h-[40px] w-[290px] mt-5 bg-[#ffffff]" style={{ marginLeft: "33px" }}>
+            <Link href={href}> {/* Use the href prop */}
+                <div className="grid grid-cols-11">
+                    <img src={gambar} alt={judul} style={{ width: "23px", height: "23px", marginLeft: "20px", marginTop: "7px" }} />
+                    <p className="text-black" style={{ marginLeft: "40px", marginTop: "8px" }}>{judul}</p>
+                </div>
             </Link>
         </nav>
-        
     )
 }
 
-export default Navbar
+export default Navbar;
