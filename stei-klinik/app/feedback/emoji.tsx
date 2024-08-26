9d2e64b9377aaa7d63aedd8fbfecd114bbf7404a
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-08-26 20:51:38 +0700

    fix ui

--- stei-klinik/app/feedback/emoji.tsx
+++ stei-klinik/app/feedback/emoji.tsx
@@ -1,20 +1,16 @@
-import Link from "next/link";
 
 export interface emjProps {
     gambar: string;
     nama: string;
-    href: string;
 }
-
-const Emoji= ({gambar, nama, href}:emjProps) => {
+// tambahin untuk function booleannya kalau di click
+const Emoji= ({gambar, nama}:emjProps) => {
     return(
         <nav>
-            <Link href={href}>
-                <div className="grid grid-cols">
-                    <img src={gambar} alt={nama} style={{width:"36.81px", height:"36.81px", marginLeft:"80px", marginTop:"60px"}}/>
-                    <p className="text-black text-ms" style={{marginLeft:"78px"}}>{nama}</p>
-                </div>
-            </Link>
+            <div className="grid grid-cols">
+                <img src={gambar} alt={nama} style={{width:"36.81px", height:"36.81px", marginLeft:"78px", marginTop:"80px"}}/>
+                <p className="text-black text-ms" style={{marginLeft:"77px"}}>{nama}</p>
+            </div>
         </nav>
     )
 }
