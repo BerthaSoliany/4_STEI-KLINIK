9b6a7dd18ef92f3f08864a1b929221dfb8baf05a
ivan-wirawan <99517942+ivan-wirawan@users.noreply.github.com>
2024-08-28 23:38:45 +0700

    fix salah tulis

--- stei-klinik/app/pilih-artikel/page.tsx
+++ stei-klinik/app/pilih-artikel/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
                 />
                 <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/sakit-kepala">Sakit Kepala: Jenis, Penyebab, dan Cara Mengobatinya</Link>
                 </h3>
-                <p className="text-gray-600">21 Agustus 2024</p>
+                <p className="text-gray-600">17 Agustus 2024</p>
             </div>
             <div className="bg-[#F4FFE7] text-black flex flex-col items-center rounded shadow p-4">
                 <Image
