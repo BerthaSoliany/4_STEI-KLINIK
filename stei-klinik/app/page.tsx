e73997dbd088142da268f9102530bb85dcc26dfd
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-08-28 23:48:40 +0700

    fix: fix button

--- stei-klinik/app/page.tsx
+++ stei-klinik/app/page.tsx
@@ -1,113 +1,138 @@
 import Image from "next/image";
+import Banner from '@/public/Banner.png'
+import Logo from '@/public/Logo.png'
+import Featlink from '../app/home/featurebutton'
+import Spotlight from '@/public/Spotlight.png'
+import Artikel from '@/public/Artikel.png'
+import Header from '@/app/Header/logo'
+import Link from "next/link"
+import Footer from '@/app/Footer/footerhome'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col bg-white">
+        <div className="z-10">
+            <div className="w-full flex flex-col">
+            
+            <div className="flex justify-between items-center bg-white p-4 shadow-md">
+                <div className="flex w-40">
+                    <Header />
+                </div>
+
+                <div className="flex space-x-4 pr-2">
+                    <Link href="/login">
+                        <button className="bg-[#006769] text-white px-4 py-2 rounded hover:bg-blue-600">
+                            Login
+                        </button>
+                    </Link>
+                    <Link href="/register">
+                        <button className="bg-[#006769] text-white px-4 py-2 rounded hover:bg-red-600">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        
+            
+            <div className=" flex items-center bg-[#006769] h-[71px] ">
+                <div className="ml-auto p-4">
+                <input 
+                    type="text" 
+                    placeholder="Search..." 
+                    className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" 
+                />
+                </div>
+            </div>
+            
+            </div>
         </div>
-      </div>
+        <div className="p-0">
+            <div className="flex justify-center  w-full h-full bg-white pl-16 pr-16 pt-16">
+            <div className="flex flex-col  w-screen min-w-96 bg-white rounded">
+                
+                
+                <div>
+                <Image className="w-screen py-4 rounded"
+                    src={Banner}
+                    alt="Banner"
+                />
+                </div>
+                
+             
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                <div>
+                    <div className="flex items-center space-x-4">
+                    <h1 className="text-[#006769] text-4xl font-bold pt-10 pl-10 pb-3 leading-tight">Feature</h1>
+                    <hr className="flex-1 h-1 bg-[#006769] border-0 rounded mt-8" />
+                </div>
+                <div className="flex flex-wrap justify items-center">
+                    <Featlink featlink="https://student.itb.ac.id/bk/peer" featcolor="solid" featname="Pendaming Sebaya ITB"/>
+                    <Featlink featlink="https://six.itb.ac.id/" featcolor="solid" featname="Integrasi Direktorat"/>
+                    <Featlink featlink="/chat-dokter" featcolor="" featname="Info Kesehatan"/>
+                    <Featlink featlink="/chat-dokter" featcolor="" featname="Chat Dengan Dokter"/>
+                </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                <h1 className="text-[#006769] text-4xl font-bold pt-10 pl-10 pb-3">Artikel</h1>
+                <hr className="flex-1 h-1 bg-[#006769] border-0 rounded mt-8" />
+                </div>
+                <div className="w-full bg-[#F4FFE7] p-4 rounded mt-8 text-center flex flex-col items-center">
+                <Image className="flex items-center"
+                    src ={Spotlight}
+                    alt = "Spotlight"
+                />
+                <h2 className="text-3xl text-black font-semibold hover:text-blue-400"><Link href="/articles/flu-dan-demam">Flu dan Demam: Gejala dan Cara Mencegahnya</Link></h2>
+                <p className="text-black mt-2">Gejala pilek dan demam sama-sama membuat badan terasa tidak karuan. Akan tetapi, dua penyakit ini bisa membuat kondisi tubuh anda memburuk meski Anda sudah minum obat.
+                <br/> Kebiasaan yang memperburuk gejala pilek dan demam: <br/>
+                1. Membiarkan sakit begitu saja Pilek dan demam tampak seperti penyakit sepele sehingga sering dihiraukan. Menghiraukan penyakit sama saja membiarkan virus dan kuman semakin menyebar dalam tubuh. Akibatnya, kekebalan tubuh Anda menjadi semakin menurun dan kemungkinan penularannya pun akan semakin besar.</p>
+                <p className="text-blue-400 mt-2"><Link href="/articles/flu-dan-demam">Selengkapnya </Link></p>
+                </div>
+                
+                <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+                <div className="bg-[#F4FFE7] p-4 rounded shadow items-center flex flex-col">
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/sakit-tenggorokan">Sakit Tenggorokan: Penyebab, Gejala, dan Pengobatan Efektif</Link></h3>
+                    <p className="text-gray-600">20 Agustus 2024</p>
+                </div>
+                <div className="bg-[#F4FFE7] p-4 rounded shadow items-center flex flex-col">
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/sakit-kepala ">Sakit Kepala: Jenis, Penyebab, dan Cara Mengobatinya</Link>
+                    </h3>
+                    <p className="text-gray-600">21 Agustus 2024</p>
+                </div>
+                <div className="bg-[#F4FFE7] p-4 rounded shadow items-center flex flex-col">
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/penyebab-nyeri">Penyebab Umum Nyeri pada Tangan dan Cara Pencegahannya</Link></h3>
+                    <p className="text-gray-600">22 Agustus 2024</p>
+                </div>
+                <div className="bg-[#F4FFE7] p-4 rounded shadow items-center flex flex-col">
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/covid-19-masa-kini">Covid-19 Masa Kini</Link></h3>
+                    <p className="text-gray-600">23 Agustus 2024</p>
+                </div>
+                </div>
+                <br/>
+                <button className=" rounded-full border-2 border-[#006769] px-4 py-2 text-[#006769] font-bold hover:border-blue-400 hover:text-blue-400 m-4">
+                <Link href = "/pilih-artikel">Lihat Berita Lainnya</Link>
+                </button>   
+                
+            </div>
+            
+        </div>
+        <Footer></Footer>    
+    </div>
+    </div>
   );
 }
