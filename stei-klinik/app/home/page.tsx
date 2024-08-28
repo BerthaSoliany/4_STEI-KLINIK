fc922ca13375940e96f08f08971d9d1d70327e24
ivan-wirawan <ivanwirawan25@gmail.com>
2024-08-28 15:52:51 +0700

    push

--- stei-klinik/app/home/page.tsx
+++ stei-klinik/app/home/page.tsx
@@ -4,7 +4,8 @@ import Logo from '@/public/Logo.png'
 import Featlink from './featurebutton'
 import Spotlight from '@/public/Spotlight.png'
 import Artikel from '@/public/Artikel.png'
-
+import Header from '@/app/Header/logo'
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -14,16 +15,9 @@ export default function Home() {
             
             <div className="flex justify-between items-center bg-white p-4 shadow-md">
                 <div className="flex pl-10">
-                <Image 
-                src={Logo} 
-                alt="Logo" 
-                width={50} 
-                height={50} 
-                />
-                <h1 className="text-[#006769] text-3xl font-bold pt-2">STEI-Klinik</h1>
+                <Header />
                 </div>
 
-                
                 <div className="flex space-x-4 pr-10">
                 <button className="bg-[#006769] text-white px-4 py-2 rounded hover:bg-blue-600">
                     Login
@@ -35,12 +29,12 @@ export default function Home() {
             </div>
         
             
-            <div className=" flex items-center bg-[#006769] h-[71px]">
-                <div className="w-full max-w-2xl p-4 ">
+            <div className=" flex items-center bg-[#006769] h-[71px] ">
+                <div className="ml-auto p-4">
                 <input 
                     type="text" 
                     placeholder="Search..." 
-                    className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" 
+                    className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" 
                 />
                 </div>
             </div>
@@ -59,48 +53,68 @@ export default function Home() {
                 />
                 </div>
                 
+             
+
                 <div>
-                <h1 className="text-[#006769] text-4xl font-bold pt-10 pl-10 pb-3">Feature</h1>
-                <div className="flex flex-wrap">
+                <div className="flex items-center space-x-4">
+                <h1 className="text-[#006769] text-4xl font-bold pt-10 pl-10 pb-3 leading-tight">Feature</h1>
+                <hr className="w-80 h-1 bg-[#006769] border-0 rounded mt-8" />
+                </div>
+                <div className="flex flex-wrap justify items-center">
                     <Featlink featlink="https://student.itb.ac.id/bk/peer" featcolor="solid" featname="Pendaming Sebaya ITB"/>
                     <Featlink featlink="https://six.itb.ac.id/" featcolor="solid" featname="Integrasi Direktorat"/>
                     <Featlink featlink="/chat-dokter" featcolor="" featname="Info Kesehatan"/>
                     <Featlink featlink="/chat-dokter" featcolor="" featname="Chat Dengan Dokter"/>
                 </div>
                 </div>
-
+                <div className="flex items-center space-x-4">
+                <h1 className="text-[#006769] text-4xl font-bold pt-10 pl-10 pb-3">Artikel</h1>
+                <hr className="w-80 h-1 bg-[#006769] border-0 rounded mt-8" />
+                </div>
                 <div className="w-full bg-[#d8f691] p-4 rounded mt-8 text-center flex flex-col items-center">
                 <Image className="flex items-center"
                     src ={Spotlight}
                     alt = "Spotlight"
                 />
-                <h2 className="text-3xl text-black font-semibold">Flu dan Demam: Gejala dan Cara Mencegahnya</h2>
+                <h2 className="text-3xl text-black font-semibold hover:text-blue-400"><Link href="/articles/flu-dan-demam">Flu dan Demam: Gejala dan Cara Mencegahnya</Link></h2>
                 <p className="text-black mt-2">Gejala pilek dan demam sama-sama membuat badan terasa tidak karuan. Akan tetapi, dua penyakit ini bisa membuat kondisi tubuh anda memburuk meski Anda sudah minum obat.
-
-        Kebiasaan yang memperburuk gejala pilek dan demam:
-
-
-        1. Membiarkan sakit begitu saja
-
-        Pilek dan demam tampak seperti penyakit sepele sehingga sering dihiraukan. Menghiraukan penyakit sama saja membiarkan virus dan kuman semakin menyebar dalam tubuh. Akibatnya, kekebalan tubuh Anda menjadi semakin menurun dan kemungkinan penularannya pun akan semakin besar.</p>
+                <br/> Kebiasaan yang memperburuk gejala pilek dan demam: <br/>
+                1. Membiarkan sakit begitu saja Pilek dan demam tampak seperti penyakit sepele sehingga sering dihiraukan. Menghiraukan penyakit sama saja membiarkan virus dan kuman semakin menyebar dalam tubuh. Akibatnya, kekebalan tubuh Anda menjadi semakin menurun dan kemungkinan penularannya pun akan semakin besar.</p>
+                <p className="text-blue-400 mt-2"><Link href="/articles/flu-dan-demam">Selengkapnya </Link></p>
                 </div>
                 
                 <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
                 <div className="bg-[#d8f691] p-4 rounded shadow">
-                    <h3 className="text-black font-bold">Sakit Tenggorokan: Penyebab, Gejala, dan Pengobatan Efektif</h3>
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/sakit-tenggorokan">Sakit Tenggorokan: Penyebab, Gejala, dan Pengobatan Efektif</Link></h3>
                     <p className="text-gray-600">20 Agustus 2024</p>
                 </div>
                 <div className="bg-[#d8f691] p-4 rounded shadow">
-                    <h3 className="text-black font-bold">Sakit Kepala: Jenis, Penyebab, dan Cara Mengobatinya
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/sakit-kelapa">Sakit Kepala: Jenis, Penyebab, dan Cara Mengobatinya</Link>
                     </h3>
                     <p className="text-gray-600">21 Agustus 2024</p>
                 </div>
                 <div className="bg-[#d8f691] p-4 rounded shadow">
-                    <h3 className="text-black font-bold">Penyebab Umum Nyeri pada Tangan dan Cara Pencegahannya</h3>
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/penyebab-nyeri">Penyebab Umum Nyeri pada Tangan dan Cara Pencegahannya</Link></h3>
                     <p className="text-gray-600">22 Agustus 2024</p>
                 </div>
                 <div className="bg-[#d8f691] p-4 rounded shadow">
-                    <h3 className="text-black font-bold">Covid-19 Masa Kini</h3>
+                    <Image
+                        src ={Artikel}
+                        alt = "Artikel"
+                    />
+                    <h3 className="text-black font-bold hover:text-blue-400"><Link href="/articles/covid-19-masa-kini">Covid-19 Masa Kini</Link></h3>
                     <p className="text-gray-600">23 Agustus 2024</p>
                 </div>
                 </div>
